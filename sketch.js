0914aea9f3cb55e4e1ab45643ce9f4abb31bf033
jannyyyy1 <jannyzzz@naver.com>
2025-02-22 01:01:24 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,8 +19,8 @@ function setup() {
   coinY = height / 2;
 
   // 은화의 초기 속도 (기본 속도 설정)
-  coinSpeedX = 10;  // X축 속도
-  coinSpeedY = 10;  // Y축 속도
+  coinSpeedX = 20;  // X축 속도
+  coinSpeedY = 20;  // Y축 속도
 }
 
 function draw() {
